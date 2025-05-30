5391bd7831c29f32e1cd638b4f83a71c4298f969
dylanO9 <dylan.olivares11@gmail.com>
2025-05-29 20:52:46 -0700

    Routes alignment to specification

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,8 @@ const userRoutes = require('./routes/users');
 const journalRoutes = require('./routes/journal');
 const contactRoutes = require('./routes/contacts');
 
-app.use('/api/users', userRoutes);
-app.use('/api/journal', journalRoutes);
-app.use('/api/contacts', contactRoutes);
+app.use('/users', userRoutes);
+app.use('/journal', journalRoutes);
+app.use('/contacts', contactRoutes);
 
 module.exports = app;
